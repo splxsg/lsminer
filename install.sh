ed060789b0197d19b6e7412b5f35fbb4c858f6ae
wtxpwh <wtxpwh@163.com>
2020-01-17 18:45:56 +0800

    add tmux config file

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ echo ""
 echo "Files copying..."
 echo ""
 cp -f /home/lsminer/lsminer/etc/minerscreen.desktop /home/lsminer/.config/autostart/minerscreen.desktop
+cp -f /home/lsminer/lsminer/etc/tmux.conf /home/lsminer/.tmux.conf
 #cp -f /home/lsminer/lsminer/etc/wallpaper.jpg /home/lsminer/wallpaper.jpg
 sudo cp -f /home/lsminer/lsminer/etc/prepare.service /etc/systemd/system/prepare.service
 sudo cp -f /home/lsminer/lsminer/etc/miner.service /etc/systemd/system/miner.service
